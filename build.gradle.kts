5cf7b6f0c54585e4ddbb0815ab662714dbc6d03f
mvukic <matija.vukic2@gmail.com>
2024-11-30 12:59:58 +0100

    u

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.internal.config.LanguageFeature
+
 plugins {
     kotlin("jvm") version "2.1.0"
 }
@@ -7,4 +9,20 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+kotlin {
+    sourceSets.all {
+        languageSettings {
+            enableLanguageFeature(LanguageFeature.WhenGuards.name)
+            enableLanguageFeature(LanguageFeature.MultiDollarInterpolation.name)
+            enableLanguageFeature(LanguageFeature.BreakContinueInInlineLambdas.name)
+        }
+    }
+}
+
+tasks {
+    wrapper {
+        version = "8.11"
+    }
 }
