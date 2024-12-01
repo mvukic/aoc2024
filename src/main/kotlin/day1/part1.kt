20e58de6d31f9096adefe2a685703f3d708ec653
mvukic <matija.vukic2@gmail.com>
2024-12-01 11:54:15 +0100

    u

--- src/main/kotlin/day1/part1.kt
+++ src/main/kotlin/day1/part1.kt
@@ -5,7 +5,7 @@ import kotlin.math.abs
 
 fun main() {
     val lines = linesForDay(1)
-    val pairs = lines.map { it.split("   ") }
+    val pairs = lines.map { it.split("\\s+".toRegex()) }
     val left = pairs.map { it[0].toInt() }.sorted()
     val right = pairs.map { it[1].toInt() }.sorted()
     val distances = left.zip(right).map { abs(it.first - it.second) }
--- src/main/kotlin/day1/part2.kt
+++ src/main/kotlin/day1/part2.kt
@@ -4,7 +4,7 @@ import com.github.mvukic.linesForDay
 
 fun main() {
     val lines = linesForDay(1)
-    val pairs = lines.map { it.split("   ") }
+    val pairs = lines.map { it.split("\\s+".toRegex()) }
     val left = pairs.map { it[0].toInt() }
     val right = pairs.map { it[1].toInt() }.groupingBy { it }.eachCount()
     val distances = left.map { it * (right[it] ?: 0) }
