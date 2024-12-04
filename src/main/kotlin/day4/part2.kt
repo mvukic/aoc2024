54ee5dc0fb6720f6f0aacaa7a2888d7df248b1e8
mvukic <matija.vukic2@gmail.com>
2024-12-04 22:43:35 +0100

    u

--- src/main/kotlin/day4/part2.kt
+++ src/main/kotlin/day4/part2.kt
@@ -5,3 +5,32 @@ import com.github.mvukic.linesForDay
 fun main() {
     val lines = linesForDay(4).map { it.toCharArray() }
 }
+
+fun countSubmatrixOccurrences(matrix: Array<CharArray>, submatrix: Array<CharArray>): Int {
+    val rows = matrix.size
+    val cols = matrix[0].size
+    val subRows = submatrix.size
+    val subCols = submatrix[0].size
+    var count = 0
+
+    for (i in 0 until rows - subRows + 1) {
+        for (j in 0 until cols - subCols + 1) {
+            var found = true
+            for (k in 0 until subRows) {
+                for (l in 0 until subCols) {
+                    if (matrix[i + k][j + l] != submatrix[k][l]) {
+                        found = false
+                        break
+                    }
+                }
+                if (!found) {
+                    break
+                }
+            }
+            if (found) {
+                count++
+            }
+        }
+    }
+    return count
+}
